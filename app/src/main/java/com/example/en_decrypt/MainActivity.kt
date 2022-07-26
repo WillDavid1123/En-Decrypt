9a745e5c3e969f51e1ca8d7ba3ef7e354ff1c4bc
William Davidson <william11231999@gmail.com>
2022-07-25 23:31:53 -0400

    Added top appbar to Activity

--- app/src/main/java/com/example/en_decrypt/MainActivity.kt
+++ app/src/main/java/com/example/en_decrypt/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.en_decrypt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainToolbar = findViewById<Toolbar>(R.id.mainToolbar)
+        setSupportActionBar(mainToolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
